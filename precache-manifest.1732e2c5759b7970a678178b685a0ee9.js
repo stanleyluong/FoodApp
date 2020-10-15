aff084680cbd6fd7f92fce7d56997f8dec7e0e84
Stanley Luong <stanleyluong@Stanleys-MacBook-Pro.local>
2020-10-15 00:22:25 -0700

    Updates

---
+++ precache-manifest.1732e2c5759b7970a678178b685a0ee9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6b8f65b7ad4233df95b26d063f4cc0",
+    "url": "/foodapp/index.html"
+  },
+  {
+    "revision": "a012a76a2d254698d193",
+    "url": "/foodapp/static/css/main.ea65b9f1.chunk.css"
+  },
+  {
+    "revision": "575f8d6868c66ece0d80",
+    "url": "/foodapp/static/js/2.4a40e4ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foodapp/static/js/2.4a40e4ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a012a76a2d254698d193",
+    "url": "/foodapp/static/js/main.56c158c0.chunk.js"
+  },
+  {
+    "revision": "f0bf9c9668634a2861e0",
+    "url": "/foodapp/static/js/runtime-main.f97895e9.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/foodapp/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/foodapp/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
