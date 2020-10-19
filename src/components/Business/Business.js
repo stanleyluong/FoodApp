b6c28dbc7ee839c7f06ffa20319e1bdba1ba466e
Stanley Luong <stanleyluong@Stanleys-MacBook-Pro.local>
2020-10-19 14:55:14 -0700

    added google maps link

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -2,6 +2,9 @@ import React from 'react'
 import './Business.css'
 class Business extends React.Component {
     render(){
+        let formattedAddress = [this.props.business.location.address1,this.props.business.location.address2,this.props.business.location.address3,this.props.business.location.city,this.props.business.location.state,this.props.business.location.zip_code].filter(address=>{return address!==""}).join(' ').split(' ').join('+')
+        console.log(formattedAddress,'formatted')
+        
         return (
             <div className="Business">
                 <div className="image-container">
@@ -10,12 +13,14 @@ class Business extends React.Component {
                 <h2>{this.props.business.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
-                        <p>{this.props.business.location.address1}</p>
-                        <p>{this.props.business.location.address2}</p>
-                        <p>{this.props.business.location.address3}</p>
-                        <p>{this.props.business.location.city} {this.props.business.location.state}</p>
-                        <p>{this.props.business.location.zip_code}</p>
-                        <p>{Math.floor(this.props.business.distance)} meters away</p>
+                        <a target="_blank" rel="noopener noreferrer" href={`http://maps.google.com/maps?q=${formattedAddress}`}>
+                            <p>{this.props.business.location.address1}</p>
+                            <p>{this.props.business.location.address2}</p>
+                            <p>{this.props.business.location.address3}</p>
+                            <p>{this.props.business.location.city} {this.props.business.location.state}</p>
+                            <p>{this.props.business.location.zip_code}</p>
+                        </a>
+                            <p>{Math.floor(this.props.business.distance)} meters away</p>
                     </div>
                     <div className="Business-reviews">
                         <h3>{this.props.business.categories[0].title}</h3>
