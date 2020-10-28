05cc63d4171fc3dd5aee6ebf8da30cd6edf84ab8
Stanley Luong <stanleyluong@Stanleys-MacBook-Pro.local>
2020-10-27 22:58:44 -0700

    fixed review div and css

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -6,11 +6,11 @@ class Review extends React.Component{
         return (
             <div className="Review">
                 {/* <h4>{this.props.review}</h4> */}
-                <img style={{width:"100%"}} src={this.props.review.user.image_url} alt="oops"></img>
+                <img style={{width:"100%"}} src={this.props.review.user.image_url} alt="No User Photo"></img>
                 {/* <p>ID: {this.props.review.id}</p> */}
                 <p>Rating: {this.props.review.rating} Stars</p>
-                <p>Review: {this.props.review.text}</p>
-                <p>Time: {this.props.review.time_created}</p>
+                <p>"{this.props.review.text}"</p>
+                <p>Review Date: {this.props.review.time_created}</p>
                 {/* <p>User: {this.props.review.user.id}</p> */}
                 
                 
