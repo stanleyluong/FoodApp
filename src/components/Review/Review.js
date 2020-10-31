9bba109a711192b13f1d78dbb6dbe375ae8fce17
Stanley Luong <stanleyluong@Stanleys-MacBook-Pro.local>
2020-10-30 17:11:17 -0700

    cleaned up error messages

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -5,15 +5,10 @@ class Review extends React.Component{
         console.log(this.props.review)
         return (
             <div className="Review">
-                {/* <h4>{this.props.review}</h4> */}
-                <img style={{width:"100%"}} src={this.props.review.user.image_url} alt="No User Photo"></img>
-                {/* <p>ID: {this.props.review.id}</p> */}
+                <img style={{width:"100%"}} src={this.props.review.user.image_url} alt=""></img>
                 <p>Rating: {this.props.review.rating} Stars</p>
                 <p>"{this.props.review.text}"</p>
-                <p>Review Date: {this.props.review.time_created}</p>
-                {/* <p>User: {this.props.review.user.id}</p> */}
-                
-                
+                <p>Review Date: {this.props.review.time_created}</p>                
             </div>
         )
     }
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Yelp from '../../util/Yelp'
 import Review from '../Review/Review'
 
